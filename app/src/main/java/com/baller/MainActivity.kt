25fd086453b1aa87b0f3064de6504e8e5ffd7323
egelsia <hakan.karaduman@ozu.edu.tr>
2024-11-19 22:17:25 +0300

    Initial Commit

---
+++ app/src/main/java/com/baller/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.baller
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        val token = "OmVVL6nM5b2biT30RBExu4gaAHXGmlKlEbEe1S7LO4z0eBsx4lct02IGNczj"
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
